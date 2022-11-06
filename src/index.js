db7fe60658e746fe4481e63b9ba63c3a981ad2a8
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2022-11-06 19:58:58 -0300

    Componentes com propriedade

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-  <div><Primeiro />
-  <p>Seja bem vindo</p>
+  <div>
+    <Primeiro />
+    <p>Seja bem vindo</p>
+    <ComParametro
+      titulo="Segundo Componente"
+      subtitulo="Muito legal"
+      aluno="Cleiton"
+      nota={8}
+    />
   </div>,
   document.getElementById('root')
 )
